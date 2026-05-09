c5f812698e553c8837d8066f034bdb6552bc6898
sonyaadmin120 <sonyatiktok120@gmail.com>
2026-05-09 03:04:55 -0500

    cdsc

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Vercel Image Optimization ko Globally Band (Kill) kar diya
   images: {
-    unoptimized: true, // <--- YE WOH JADOO HAI JO BILL ZERO RAKHEGA
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'media.sj10.pk' },
       { protocol: 'https', hostname: 'res.cloudinary.com' },
@@ -12,28 +11,6 @@ const nextConfig = {
       { protocol: 'https', hostname: 'content.public.markaz.app' },
       { protocol: 'https', hostname: 'static.markaz.app' }
     ],
-  },
-
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'unsafe-none',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
-          }
-        ],
-      },
-    ];
   },
 };
 
